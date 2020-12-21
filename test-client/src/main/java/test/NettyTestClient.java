fe95ae8562b4317cb9404d8760b79fc87249f08f
CN-CoderLeo <321856067@qq.com>
2020-12-21 17:26:39 +0800

    增加基于Protostuff的ProtoBuf协议的序列化器

--- test-client/src/main/java/test/NettyTestClient.java
+++ test-client/src/main/java/test/NettyTestClient.java
@@ -6,12 +6,13 @@ import rpc.api.HelloService;
 import rpc.RpcClientProxy;
 import rpc.netty.client.NettyClient;
 import rpc.serializer.KryoSerializer;
+import rpc.serializer.ProtobufSerializer;
 
 public class NettyTestClient {
     public static void main(String[] args) {
 
         RpcClient client = new NettyClient("127.0.0.1", 9999);
-        client.setSerializer(new KryoSerializer());
+        client.setSerializer(new ProtobufSerializer());
         RpcClientProxy rpcClientProxy = new RpcClientProxy(client);
         HelloService helloService = rpcClientProxy.getProxy(HelloService.class);
         HelloObject object = new HelloObject(12, "This is a message");
--- test-server/src/main/java/test/NettyTestServer.java
+++ test-server/src/main/java/test/NettyTestServer.java
@@ -6,6 +6,7 @@ import rpc.netty.server.NettyServer;
 import rpc.regisitry.DefaultServiceRegistry;
 import rpc.regisitry.ServiceRegistry;
 import rpc.serializer.KryoSerializer;
+import rpc.serializer.ProtobufSerializer;
 
 public class NettyTestServer {
     public static void main(String[] args) {
@@ -13,7 +14,7 @@ public class NettyTestServer {
         ServiceRegistry registry = new DefaultServiceRegistry();
         registry.register(helloService);
         NettyServer server = new NettyServer();
-        server.setSerializer(new KryoSerializer());
+        server.setSerializer(new ProtobufSerializer());
         server.start(9999);
 
     }
